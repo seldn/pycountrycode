eb68b2e1c7f38663fd80eff75c5671c130f5de98
Vincent Arel-Bundock <varel@umich.edu>
2014-06-11 11:36:56 -0400

    radical simplification with pandas

--- countrycode/countrycode.py
+++ countrycode/countrycode.py
@@ -1,6 +1,5 @@
 import re
 import os
-import csv
 import pandas as pd
 from copy import copy
 from functools import reduce
@@ -8,61 +7,12 @@ from functools import reduce
 pkg_dir, pkg_filename = os.path.split(__file__)
 data_path = os.path.join(pkg_dir, "data", "countrycode_data.csv")
 data = pd.read_csv(data_path)
-#f = csv.reader(open(data_path, 'r'))
-#data = list(zip(*f))
-#data = [(x[0], x[1:]) for x in data]
-#data = dict(data)
-
 data_path = os.path.join(pkg_dir, "data", "cown2014.csv")
 cown = pd.read_csv(data_path)
 data_path = os.path.join(pkg_dir, "data", "cowc2014.csv")
 cowc = pd.read_csv(data_path)
-data_path = os.path.join(pkg_dir, "data", "states2014.csv")
-states = pd.read_csv(data_path)
 
-#f = lambda x: xrange(x['styear'], x['endyear'] + 1)
-#g = lambda x: reduce(lambda z, y: z + y, x)
-#def make_frame(years, code='AFG', codename='cowc'):
-    #years = list(set(sum(years, [])))
-    #out = pd.DataFrame(years, columns=['year'])
-    #out[codename] = code
-    #return out
-#cowc = []
-#cown = []
-#for c in states.stateabb.unique():
-    #tmp = states[states.stateabb==c]
-    #tmp = tmp.apply(f, axis=1).tolist()
-    #tmp = [list(x) for x in tmp]
-    #tmp = make_frame(tmp, code=c, codename='cowc')
-    #cowc.append(tmp)
-#for c in states.ccode.unique():
-    #tmp = states[states.ccode==c]
-    #tmp = tmp.apply(f, axis=1).tolist()
-    #tmp = [list(x) for x in tmp]
-    #tmp = make_frame(tmp, code=c, codename='cown')
-    #cown.append(tmp)
-#cowc = pd.concat(cowc, ignore_index=True)
-#cown = pd.concat(cown, ignore_index=False)
-
-
-def countryyear(code='iso3c', years=list(range(1990,2013))):
-    if not years:
-        if code == 'cown':
-            out = cown
-        else:
-            out = cowc
-    else:
-        codes = data[code]
-        out = pd.DataFrame(list(zip(list(range(len(codes))), codes)), columns=['idx', code])
-        out[code][out[code]==''] = None
-        out = out.dropna()
-        out = [out.copy() for x in years]
-        for i,v in enumerate(years):
-            out[i]['year'] = v
-        out = reduce(lambda x,y: pd.concat([x,y]), out)
-    return out
-
-def countrycode(codes=['DZA', 'CAN'], origin='iso3c', target='country_name', dictionary=False):
+def countrycode(codes=['DZA', 'CAN'], origin='iso3c', target='country_name'):
     '''Convert to and from 11 country code schemes. Use regular expressions to
     detect country names and standardize them. Assign region/continent
     descriptors.
@@ -101,60 +51,44 @@ def countrycode(codes=['DZA', 'CAN'], origin='iso3c', target='country_name', dic
         * continent : Name of continent
     '''
 
-    if dictionary:
-        in_codes = pd.Series(codes).unique()
-        out_codes = _convert(in_codes, origin, target)
-        out = pd.DataFrame(list(zip(in_codes, out_codes)), columns=[origin, target])
-        out = out.dropna()
+    if type(codes) in (list, str):
+        input_codes = pd.Series(codes)
+    elif type(codes) == pd.core.series.Series:
+        input_codes = codes
     else:
-        out = _convert(codes, origin, target)
-    return out
-
-def _convert(codes, origin, target):
-    '''Internal conversion function'''
-
-    # Codes to be converted (cleanup)
-    if type(codes) in [str, str, int]:
-        codes = [codes]
-        loner = True
-    else:
-        loner = False
-
-    try:
-        codes = ["%.0f" % x for x in codes]
-    except:
-        codes = [str(x).strip() for x in codes]
-
-    # Dictionary
-    target_codes = data[target]
+        raise TypeError('codes must be string, list, or pandas series')
 
     if origin == 'country_name':
-        origin_codes = ['(?i)' + x for x in data['regex']]
+        origin = 'regex'
+        data['regex'] = '(?i)'+ data['regex']
+
+    dictionary = data[[origin, target]].dropna()
+    dictionary = dict(zip(data[origin], data[target]))
+    if origin != 'regex':
+        output_codes = input_codes.replace(dictionary, regex=False)
     else:
-        origin_codes = data[origin]
+        output_codes = input_codes.replace(dictionary, regex=True)
 
-    #idx = [True if (v not in ['NA','']) and (origin_codes[i] not in ['NA','']) else False
-    idx = [True if (v != '') and (origin_codes[i] != '') else False
-           for i,v in enumerate(target_codes)]
+    if type(codes) == list:
+        output_codes = output_codes.tolist()
+    elif type(codes) == str:
+        output_codes = output_codes[0]
 
-    origin_codes = [v for i,v in enumerate(origin_codes) if idx[i]]
-    target_codes = [v for i,v in enumerate(target_codes) if idx[i]]
+    return output_codes
 
-    dictionary = dict(list(zip(origin_codes, target_codes)))
-
-    if origin != 'country_name':
-        codes_new = ["None" if x not in origin_codes else x for x in codes]
+def countryyear(code='iso3c', years=list(range(1990,2013))):
+    if not years:
+        if code == 'cown':
+            out = cown
+        else:
+            out = cowc
     else:
-        codes_new = copy(codes)
-
-    for k in list(dictionary.keys()):
-        codes_new = [dictionary[k] if re.match('^'+k+'$', x) != None else x
-                     for x in codes_new]
-
-    # Output
-    codes_new = [None if x=='None' else x for x in codes_new]
-
-    if loner:
-        codes_new = codes_new[0]
-
-    return codes_new
+        codes = data[code]
+        out = pd.DataFrame(list(zip(list(range(len(codes))), codes)), columns=['idx', code])
+        out[code][out[code]==''] = None
+        out = out.dropna()
+        out = [out.copy() for x in years]
+        for i,v in enumerate(years):
+            out[i]['year'] = v
+        out = reduce(lambda x,y: pd.concat([x,y]), out)
+    return out
